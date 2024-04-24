77499ec6d01a63ba509b69c4d0d38ab7af1d7da8
Radha Gunturu <radheram_a@yahoo.com>
2024-04-24 14:35:19 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ console.log('This is NodeJS, Express and MongoDB app');
 
 const express = require('express');
 const path = require('path');  // for handling file paths
+const da = require("./data-access");
+
 
 const app = express();
 const port = process.env.PORT || 4000;  // use env var or default to 4000
@@ -13,3 +15,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+app.get("/customers", async (req, res) => {
+  const cust= await da.getCustomers();
+  res.send(cust); 
+ });
